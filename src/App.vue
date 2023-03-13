103d8281db7e099d3a0b2ca9fddf9fa4d41959b1
Mangesh Landge <mangesh.landge@gmail.com>
2023-03-13 17:46:43 +0530

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Home />
+    <Home data="Mangesh is here" msg="Hi from Mangesh" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    Home
   }
 }
 </script>
---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+
+    <h1>Hello from home component</h1>
+    <h2>{{ data }}</h2>
+    <p>{{ msg }}</p>
+  </div>
+
+</template>
+
+<script >
+  export default {
+    name: 'Home',
+    props: {
+      data: String,
+      msg: String
+    }
+  }
+</script> 
