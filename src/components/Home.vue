1c7fb161db46080f12de82ebb58aa318f2ed74d9
Mangesh Landge <mangesh.landge@gmail.com>
2023-03-13 18:15:43 +0530

    conditional rendering

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,15 @@
 <template>
   <div>
 
-    <h1>Hello from home component</h1>
+    <h1>Conditional rendering</h1>
     <!-- <h2>{{ data }}</h2>
     <p>{{ msg }}</p> -->
     <!-- v-on is used for button events -->
-    <button v-on:click="hello('Click')">Call Click Btn</button>
-    <button v-on:mouseover="hello('MouseOver')">Call mouseover Btn</button>
+    <!-- <button v-on:click="hello('Click')">Call Click Btn</button>
+    <button v-on:mouseover="hello('MouseOver')">Call mouseover Btn</button> -->
+    <h1 v-if="show">Conditional rendering if block</h1>
+    <h1 v-else>Conditional rendering in else block</h1>
+    <button v-on:click="display">Toggle Element</button>
   </div>
 
 </template>
@@ -18,11 +21,19 @@
       data: String,
       msg: String
     },
+    data(){
+      return {show: true}
+    },
     methods:{
-      hello(data){
-        console.log('BTN')
-        alert(data)
+      // hello(data){
+      //   console.log('BTN')
+      //   alert(data)
+      // }
+      display(){
+        this.show = !this.show;
       }
-    }
+    },
+    
+    
   }
 </script> 
